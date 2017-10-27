2eb6712b1736d835cdd2d05bc98d027468c6a9c4
torrent benoit <torrentdu64@gmail.com>
2017-10-27 17:29:23 +0200

    refacto route passport

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+
 
 
 
@@ -10,20 +9,9 @@ const keys = require('./config/keys');
 const app = express();
 
 
+require('./routes/authRoutes')(app);
 
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
 
-}));
-
-app.get('auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}));
-
-app.get('auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
