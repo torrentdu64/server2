500493811e50051c45cf7159ef24f1a7f2549f65
torrent benoit <torrentdu64@gmail.com>
2017-10-30 15:59:01 +0100

    remote mongoDB and mongoose orm

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
 
-
+mongoose.connect(keys.mongoURI);
 
 
 
