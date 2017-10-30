c1769eacc7c4308954a9cedee58c505417ef163d
torrent benoit <torrentdu64@gmail.com>
2017-10-30 18:22:11 +0100

    add promise to record user

--- services/passport.js
+++ services/passport.js
@@ -15,9 +15,10 @@ passport.use(new GoogleStrategy({
       User.findOne({ googleId: profile.id})
         .then( (existingUser) => {
             if(existingUser){
-
+              done(null, existingUser);
             }else{
-              new User({ googleId: profile.id }).save();
+              new User({ googleId: profile.id }).save()
+                .then( user => done(null, user));
             }
         })
 
