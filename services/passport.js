82f265ca7a3ed5a78641b4233c2b230397eef37b
torrent benoit <torrentdu64@gmail.com>
2017-10-31 15:33:00 +0100

    set cookie session done

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
+
+
 const keys = require('../config/keys');
 
 
