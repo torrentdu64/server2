dda736fbb57995bd855728e2f4cba8a8864ebdf4
torrent benoit <torrentdu64@gmail.com>
2017-11-03 15:56:38 +0100

    store and provider implement

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
 
 
-ReactDOM.render();
+import App from './components/App';
+
+const store = createStore( () => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+   document.querySelector('#root')
+   );
