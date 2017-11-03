af8c773f1f0f616e3eaae8b1365b2c740c196f33
torrent benoit <torrentdu64@gmail.com>
2017-11-03 16:39:28 +0100

    header build

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import { browserRouter, Route } from 'react-router-dom';
+
+const  Header = () => <h2>header</h2>;
+const  Dashboard = () => <h2>Dashboard</h2>;
+const  SurveyNew = () => <h2>SurveyNew</h2>;
+const  Landing = () => <h2>Landing</h2>;
+
 
 const App = () => {
   return (
