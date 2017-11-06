65956cdb2527fd7114df28aaa723de7eaace2351
torrent benoit <torrentdu64@gmail.com>
2017-11-06 11:16:38 +0100

    refacto dispatch methode

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,10 +3,8 @@ import { FETCH_USER } from './types';
 
 
 
-const fetchUser = () => {
-  return function(dispatch) {
+export const fetchUser = () => async dispatch => {
+   const res =  await axios.get('/api/current_user');
+    dispatch({ type: FETCH_USER, payload: res});
+};
 
-    axios.get('/api/current_user')
-    .then( res => dispatch({ type: FETCH_USER, payload: res}))
-  }
-}
