8183b30ce63eff1f36d4442cc7ad7b03cd6e2805
torrent benoit <torrentdu64@gmail.com>
2017-11-06 10:28:26 +0100

    request to back server

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,9 +4,18 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div >
-        Header
-      </div>
+      <nav >
+        <div className="nav-warpper">
+          <a className="left brand-logo">
+          Header
+          </a>
+          <ul className="right">
+            <li>
+              <a> sign in google </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
       );
   }
 }
