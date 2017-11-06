f86d53aceee0d456ce86e4ad3fdd1f9207e74274
torrent benoit <torrentdu64@gmail.com>
2017-11-06 10:41:01 +0100

    add response api to client side

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,5 +4,9 @@ import { FETCH_USER } from './types';
 
 
 const fetchUser = () => {
+  return function(dispatch) {
 
+    axios.get('/api/current_user')
+    .then( res => dispatch({ type: FETCH_USER, payload: res}))
+  }
 }
