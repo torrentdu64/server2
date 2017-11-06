93b49e81c1755334deb073952aa0b96ea6a38ac3
torrent benoit <torrentdu64@gmail.com>
2017-11-06 18:38:53 +0100

    logo redirect

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 
 class Header extends Component {
@@ -18,9 +19,9 @@ class Header extends Component {
     return (
       <nav >
         <div className="nav-warpper">
-          <a className="left brand-logo">
-          Header
-          </a>
+          <Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">
+          Emaily
+          </Link>
           <ul className="right">
             { this.renderContent()}
           </ul>
