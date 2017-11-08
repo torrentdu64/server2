39ba02e3f75018bbbaaab3a8d611bb66c5ba520a
torrent benoit <torrentdu64@gmail.com>
2017-11-08 11:06:01 +0100

    middlewares to check sing in

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,12 +1,10 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-      app.post('/api/stripe', async (req, res) => {
+      app.post('/api/stripe',requireLogin,  async (req, res) => {
 
-        if (!req.user){
-          return req.status(401).send({error: 'Login defore'});
-        }
          const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
