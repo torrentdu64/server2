42a71e847a845946f7f4a78be92ede098473bb7e
torrent benoit <torrentdu64@gmail.com>
2017-11-08 10:49:56 +0100

    add credits ad check passport

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,12 +3,18 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
       app.post('/api/stripe', async (req, res) => {
+
+        if (!req.user){
+          return req.status(401).send({error: 'Login defore'});
+        }
          const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
             description: '$5 to 5 credits',
             source: req.body.id
           });
-         console.log(charge);
+         req.user.credits += 5;
+         const user = await req.user.save();
+         res.send(user);
       });
 };
