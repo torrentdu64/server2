440668e095beb715cfe95d76a37446eef24c430a
torrent benoit <torrentdu64@gmail.com>
2017-11-10 16:58:10 +0100

    setting to construct email so nasty code

--- services/Mailer.js
+++ services/Mailer.js
@@ -10,7 +10,33 @@ class Mailer exports helper.Mail {
     this.from_email = new helper.Email('matrix@emaily.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content );
-    this.recipients = this.formatAdresses(recipients);
+    this.recipients = this.formatAddresses(recipients);
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddresses(recipients){
+      return recipients.map(({email}) => {
+          return new helper.Email(email);
+      });
+  }
+
+  addClickTracking(){
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients(){
+    const personalize = new helper.Personalization();
+
+    this.recipients.forEach( recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
   }
 
 }
