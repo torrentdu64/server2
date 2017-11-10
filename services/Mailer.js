4b7f33500d5b5118ef7e23ee2ff7048507c47a36
torrent benoit <torrentdu64@gmail.com>
2017-11-10 16:30:05 +0100

    constructor email with helper fonction

--- services/Mailer.js
+++ services/Mailer.js
@@ -4,6 +4,14 @@ const keys = require('../config/keys');
 
 
 class Mailer exports helper.Mail {
+  constructor({subject, recipients}, content) {
+    super();
+
+    this.from_email = new helper.Email('matrix@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content );
+    this.recipients = this.formatAdresses(recipients);
+  }
 
 }
 
