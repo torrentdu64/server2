6446f94c93f1cccff6628ebdc7c302c6c9416ec0
torrent benoit <torrentdu64@gmail.com>
2017-11-11 17:04:56 +0100

    new survey component

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+class SurveyNew extends Component {
+  render(){
+    return (
+      <div>
+      new
+      </div>
+      );
+  }
+}
+
+export default SurveyNew;
