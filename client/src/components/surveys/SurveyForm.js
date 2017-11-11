8c20eaef66d0fbbaec34c8deada1c499d4012939
torrent benoit <torrentdu64@gmail.com>
2017-11-11 17:25:50 +0100

    reduxForm helper inport

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+import { reduxForm } from 'redux-form';
+
+class SurveyForm extends Component {
+  render(){
+    return (
+      <div>
+      form
+      </div>
+      );
+  }
+}
+
+export default SurveyForm;
