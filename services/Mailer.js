b075baa5cc90a08e633818d21245562acca33b66
torrent benoit <torrentdu64@gmail.com>
2017-11-11 09:34:36 +0100

    finish methode to sent to api

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,7 +6,7 @@ const keys = require('../config/keys');
 class Mailer exports helper.Mail {
   constructor({subject, recipients}, content) {
     super();
-
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email('matrix@emaily.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content );
@@ -37,6 +37,17 @@ class Mailer exports helper.Mail {
       personalize.addTo(recipient);
     });
     this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: this.toJSON()
+
+    });
+    const reponse = this.sgApi.API(request);
+    return reponse;
   }
 
 }
