7606197b338fb5c4d5697261f5b77f62b84b5cd8
torrent benoit <torrentdu64@gmail.com>
2017-11-13 10:48:58 +0100

    valide and display error msg

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,15 +1,16 @@
 import _ from 'lodash';
 import React, { Component} from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 
 
 
 const FIELDS = [
-{label="survey title " , name="title" },
-{ label="subject title ", name="subject "},
-{ label="email body ", name="email" },
-{ label="email list " , name="emails" }
+{label:"survey title " , name:"title" },
+{ label:"subject title ", name:"subject "},
+{ label:"email body ", name:"email" },
+{ label:"email list " , name:"emails" }
 ];
 
 class SurveyForm extends Component {
@@ -27,14 +28,32 @@ class SurveyForm extends Component {
       <div>
       <form onSubmit={this.props.handleSubmit(value => console.log(value))}>
       {this.renderFields()}
-      <button type="submit">Submit</button>
+      <Link to="/surveys" className="btn-flat red white-text">
+      cancel
+      </Link>
+      <button type="submit" className="teal btn-flat right white-text">
+      Next
+      <i className="material-icons right">done</i>
+      </button>
       </form>
       </div>
       );
   }
 }
 
+function validate(values){
+  const errors = {};
+
+  if(!values.title){
+    errors.title = 'entrer un titre !!';
+  }
+
+  return errors;
+
+}
+
 export default reduxForm({
+  validate,
   form: 'surveyForm'
 
 })(SurveyForm);
